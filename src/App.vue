090456f07c9a0f27088754eccd7b1c2499764bc3
Nidia Bustos <ndbustos@gmail.com>
2018-06-10 19:07:59 -0400

    ciclo de vida

--- src/App.vue
+++ src/App.vue
@@ -1,69 +1,29 @@
 <template lang="pug">
   #app
-    nd-header
-
-    section.section
-      nav.nav.has-shadow
-        .container
-          h2.mb-4 {{ msg }}
-          .row
-            .col-md-6
-              input.input.form-control(
-              type="text", 
-              placeholder="Buscar canciones", v-model="searchQuery"
-              )
-            .col-md-2
-              a.btn.btn-primary.form-control(@click="search") Buscar
-            .col-md-1
-              a.btn.btn-danger.form-control &times;
-          p.text-left.mt-4
-            small {{ searchMessage }}
-
-      .container
-        .row
-          .col-md-6.results 
-            p.alert.alert-primary.text-left(v-for="t in tracks") {{ t.name }} - {{ t.artists[0].name }}
-
-
-    nd-footer
+    img(src="./assets/img/logo.png")
+    h1 Ciclo de Vida
 
 </template>
 
 <script>
 
-import trackService from './services/track'
-import ndFooter from './components/layouts/footer.vue'
-import ndHeader from './components/layouts/header.vue'
-
 export default {
   name: 'app',
 
-  components: {ndFooter, ndHeader},
-
   data () {
     return {
-      msg: 'Consumiendo APIs Rest',
-      name: '',
-      searchQuery: '',
-      tracks: []
+      person: {
+        name: 'Nidia'
+      }
     }
   },
 
-  computed: {
-    searchMessage () {
-      return `Encontrados: ${this.tracks.length}`
-    }
+  created () {
+    console.log('created....')
   },
-  methods: {
-    search () {
-      // this.tracks = tracks
-      if (!this.searchQuery) { return }
-      trackService.search(this.searchQuery)
-        .then(res => {
-          this.tracks = res.tracks.items
-          console.log(res) 
-        })
-    }
+
+  mounted () {
+    console.log('mounted....')
   }
 }
 </script>
